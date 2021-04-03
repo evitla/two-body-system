10b4f5c4cc8c46604481148b3f936afdc6a46921
Dinmukhamed <s.dinmuhamed@gmail.com>
2021-04-03 15:10:21 +0600

    fix: fix NuclearSystem to work with not only nucleons

--- nuclear_systems.py
+++ nuclear_systems.py
@@ -5,11 +5,10 @@ class NuclearSystem:
   const1 = 41.8016
   const2 = 1.439965
 
-  def __init__(self, quantum_numbers, nucleon, A, Z):
-    self.mass = A / (1 + A) / self.const1
-    self.Z = Z * self.const2 * nucleon.lower().startswith("p")
+  def __init__(self, quantum_numbers, A1, A2, Z1, Z2):
+    self.mass = A1 * A2 / (A1 + A2) / self.const1
+    self.Z = Z1 * Z2 * self.const2
     self.L, self.J = quantum_numbers.values()
-    self.nucleon = nucleon
 
   def potential(self, r):
     V = Potential(r)
@@ -17,12 +16,15 @@ class NuclearSystem:
 
 
 class Calcium40_nucleon(NuclearSystem):
-  A, Z = 40, 20
+  A1, Z1 = 40, 20
+  A2 = 1
 
   def __init__(self, quantum_numbers, nucleon):
-    super().__init__(quantum_numbers, nucleon, A=self.A, Z=self.Z)
+    Z2 = nucleon.lower().startswith("p")
+    super().__init__(quantum_numbers, self.A1, self.A2, self.Z1, Z2)
     self.V0, self.Vso = -54.823, -10
     self.a, self.r0 = 0.65, 1.2361
+    self.nucleon = nucleon
 
   def __repr__(self):
     L_letter = ["s", "p", "d", "f", "g", "h", "j"]
--- potentials.py
+++ potentials.py
@@ -5,7 +5,7 @@ class Potential:
     self.r = r
   
   def Woods_Saxon(self, nuclear_system):
-    R = nuclear_system.r0 * (nuclear_system.A + 1)**(1/3)
+    R = nuclear_system.r0 * (nuclear_system.A1 + 1)**(1/3)
     f = 1 / (1 + np.exp((self.r-R) / nuclear_system.a))
     V_central = nuclear_system.V0 * f
     LS = 0.5 * (nuclear_system.J * (nuclear_system.J + 1) - nuclear_system.L * (nuclear_system.L + 1) - 0.75)
@@ -13,7 +13,7 @@ class Potential:
     return V_central + V_LS
   
   def Coulomb(self, nuclear_system):
-    R = nuclear_system.r0 * (nuclear_system.A + 1)**(1/3)
+    R = nuclear_system.r0 * (nuclear_system.A1 + 1)**(1/3)
     Vc_1 = nuclear_system.Z / 2 / R * (3 - self.r**2 / R**2)
     Vc_2 = nuclear_system.Z / self.r
     return np.hstack((Vc_1[self.r <= R], Vc_2[self.r > R]))
